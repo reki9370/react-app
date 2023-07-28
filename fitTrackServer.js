427d94d9e8de3945aa7492425f590a8d76063b66
Reid King <77939163+reki9370@users.noreply.github.com>
2023-07-28 15:54:58 -0500

    Add files via upload

--- fitTrackServer.js
+++ fitTrackServer.js
@@ -1,3 +1,4 @@
+const secret = process.env.secret;
 const express = require('express');
 
 const cors = require('cors');
@@ -9,23 +10,62 @@ app.use(bodyParser.json());
 
 const oracledb = require('oracledb');
 
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+
+function generateToken(secret, data) {
+    const payload = { data };
+
+    const token = jwt.sign(payload, secret);
+
+    return token;
+}
+
+function verifyToken(token, secret) {
+    try {
+      const decoded = jwt.verify(token, secret);
+      return decoded;
+    } catch (error) {
+      // Token verification failed
+      console.error('Token verification failed:', error);
+      return null;
+    }
+  }
+
+function generateRandomKey(length) {
+    return crypto.randomBytes(length).toString('hex');
+}
+
+/* 
+    
+    function generateRandomKey(length) {
+    return crypto.randomBytes(length).toString('hex');
+    }
+
+    const secretKey = generateRandomKey(32); // Generate a 32-byte (256-bit) key
+    console.log(secretKey);
+
+*/
+
+
 let pool
 
+
 oracledb.createPool({
-    poolIncrement: 1,
-    poolMax: 25,
+    poolIncrement: 0,
+    poolMax: 10,
     poolMin: 10,
     queueMax: -1,
     queueTimeout: 0,
     user          : "ADMIN",
     password      : "FittrackJengA123$",
-    connectString : "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.us-phoenix-1.oraclecloud.com))(connect_data=(service_name=ga10859ba5840f5_fittrackdb_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
+    connectString : "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.us-phoenix-1.oraclecloud.com))(connect_data=(service_name=ga10859ba5840f5_fittrackdb_low.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
 }).then((newPool) => {
     pool = newPool
-    const port = 3000;
-    const ip = '192.168.67.239';
-    app.listen(port, ip, () => {
-        console.log(`Server running on port ${port}`);
+    console.log('We managed to connect to Oracle')
+    const PORT = process.env.PORT || 3030;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
     });
 });
 
@@ -75,7 +115,8 @@ app.post('/api/add-workout', (req, res) => {
 
 app.post('/api/ping-oracle-no-data', (req, res) => {
     pool.getConnection().then((connect) => {
-        connect.execute("SELECT * FROM USERINFO", {}, {maxRows: 1}).then((result) => {
+        connect.execute("SELECT 1 AS dat FROM USERINFO", {}, {maxRows: 1}).then((result) => {
+            connect.close()
             res.end(JSON.stringify(result.rows))
         }).catch((error) => {
             console.log('Errored On Query: ', error)
@@ -87,10 +128,51 @@ app.post('/api/ping-oracle-no-data', (req, res) => {
     })
 });
 
+app.post('/api/attempt-to-get', (req, res) => {
+    let email = req.body.email
+    let token = req.body.token
+
+    //Could check timestamp here. If the token is expired, force the user to log back in before they can proceed with queries. 
+
+    if((! typeof token === 'string' && ! token instanceof String) || token == null || token == undefined)
+    {
+        res.end(JSON.stringify({error: 'send valid token'}))
+    }
+    
+    let verify = verifyToken(token, secret)
+
+    if(verify.data != email)
+    {
+        console.log('User Does Not Have Access To This')
+        res.end(JSON.stringify({error: 'Access Denied'}))
+    }
+    else
+    {
+        console.log('Access Granted')
+        res.end(JSON.stringify({access: 'Access Granted'}))
+    }
+
+    /*
+    pool.getConnection().then((connect) => {
+        connect.execute("SELECT * FROM USERINFO", {}, {maxRows: 1}).then((result) => {
+            res.end(JSON.stringify(result.rows))
+        }).catch((error) => {
+            console.log('Errored On Query: ', error)
+        })
+    })
+    .catch((error) => {
+        console.log("ERROR: ", error)
+        res.end(JSON.stringify({error: 'This one errored out'}))
+    })
+    */
+});
+
+
 app.post('/api/get-profile', (req, res) => {
     let email = req.body.email;
     pool.getConnection().then((connect) => {
         connect.execute("SELECT * FROM USERINFO WHERE email = :email", {email: email}, {maxRows: 1}).then((result) => {
+            connect.close()
             res.end(JSON.stringify(result.rows))
         }).catch((error) => {
             console.log('Errored On Query: ', error)
@@ -108,32 +190,37 @@ app.post('/api/login', (req, res) => {
     let firstName = req.body.firstName
     let lastName = req.body.lastName
 
-    pool.getConnection().then((connect) => {
-        connect.execute("SELECT CASE WHEN COUNT(*) > 0 THEN 'yup' ELSE 'nope' END AS response FROM USERINFO WHERE email = :email", {email: email}, {maxRows: 1}).then((result) => {
-            if(result.rows[0][0] == 'nope')
-            {
-                //new user
-                console.log('new user login')
-                connect.execute("INSERT INTO USERINFO (USERIDENTITY, EMAIL, FULLNAME, FIRSTNAME, LASTNAME) VALUES (seq_person.nextval, :email, :fullname, :firstname, :lastname)", {email: email, fullname: fullName, firstname: firstName, lastname: lastName}, {maxRows: 1}).then((result) => { 
-                    connect.execute("COMMIT").then(() => {
-                        connect.close()
-                        res.end(JSON.stringify({userCreated: 'we created a new user'}))
+    if(email != '' && email != null && email != undefined)
+    {
+        pool.getConnection().then((connect) => {
+            connect.execute("SELECT CASE WHEN COUNT(*) > 0 THEN 'yup' ELSE 'nope' END AS response FROM USERINFO WHERE email = :email", {email: email}, {maxRows: 1}).then((result) => {
+                if(result.rows[0][0] == 'nope')
+                {
+                    //new user
+                    console.log('new user login')
+                    connect.execute("INSERT INTO USERINFO (USERIDENTITY, EMAIL, FULLNAME, FIRSTNAME, LASTNAME) VALUES (seq_person.nextval, :email, :fullname, :firstname, :lastname)", {email: email, fullname: fullName, firstname: firstName, lastname: lastName}, {maxRows: 1}).then((result) => { 
+                        connect.execute("COMMIT").then(() => {
+                            connect.close()
+                            let token = generateToken(secret, email)
+                            res.end(JSON.stringify({token: token}))
+                        })
                     })
-                })
-            }
-            else
-            {
-                //existing user
-                console.log('existing user login')
-                res.end(JSON.stringify({userCreated: 'this user already exists'}))
-            }
-            
+                }
+                else
+                {
+                    //existing user
+                    console.log('existing user login')
+                    let token = generateToken(secret, email)
+                    res.end(JSON.stringify({token: token}))
+                }
+                
+            })
         })
-    })
-    .catch((error) => {
-        console.log("ERROR: ", error)
-        res.end(JSON.stringify({error: 'This one errored out'}))
-    })
+        .catch((error) => {
+            console.log("ERROR: ", error)
+            res.end(JSON.stringify({error: 'This one errored out'}))
+        })
+    }
 
     /*
     pool.getConnection().then((connect) => {
